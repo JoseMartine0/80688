6386db0cda697863532f54f6f2f9496814427b4e
JoseMartine0 <zs19016416@estudiantes.uv.mx>
2024-10-03 13:46:30 -0600

    Practica08

---
+++ x.js
@@ -0,0 +1,15 @@
+import{y} from "./y.js"
+
+function x(params) {
+    y("x")
+    console.log("x:" + params)
+    return "fin x"
+    
+}
+
+function xx(params) {
+    return "fin xx"
+    
+}
+
+export{x, xx}
